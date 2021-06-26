0dc566b9ed96ea7923c7aa888e4c068259f59478
Samuel de Vasconcelos <samuel93vasconcelos@gmail.com>
2021-06-26 13:33:43 -0300

    3 aula - Listar perguntas da sala

--- src/pages/room/Room.tsx
+++ src/pages/room/Room.tsx
@@ -4,32 +4,60 @@ import './styles.scss';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/button/Index'
 import { RoomCode } from '../../components/roomCode/RoomCode';
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import { useAuth } from '../../hooks/useAuth';
+import toast, { Toaster } from 'react-hot-toast';
+import { database } from '../../services/firebase';
+import { useEffect } from 'react';
 
 type RoomParams = {
   id: string;
 }
 
 function Room() {
-  const user = useAuth();
+  const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState('');
   
   const roomId = params.id;
 
-  async function handleSendQuestion() {
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.once(`value`, room => {
+      console.log(room.val());
+    });
+  }, [roomId]);
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+
     if(newQuestion.trim() === '') {
       return;
     }
 
     if(!user) {
-      
+      toast.error('You must be logged in');
     }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user?.name,
+        avatar: user?.avatar,
+      },
+      isHightLighted: false,
+      isAnswered: false
+    }
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    setNewQuestion('');
   }
 
   return (
     <div id="page-room">
+      <div><Toaster /></div>
       <header>
         <div className="content">
           <img src={ logoImg } alt="Letmeask"/>
@@ -43,7 +71,7 @@ function Room() {
           <span>4 Perguntas</span>
         </div>
 
-          <form>
+          <form onSubmit={handleSendQuestion}>
             <textarea
               placeholder="O que você quer perguntar?"
               onChange={event => setNewQuestion(event.target.value)}
@@ -51,11 +79,19 @@ function Room() {
             />
 
             <div className="form-footer">
-              <span>
-                Para enviar uma pergunta, <button>faça seu login</button>
-              </span>
+              { user ? (
+                <div className="user-info">
+                  <img src={ user.avatar } alt={user.name}/>
+                  <span>{user.name}</span>
+                </div>
+              ) : (
+                <span>
+                  Para enviar uma pergunta, <button>faça seu login</button>
+                </span>
+              ) }
               <Button
                 type="submit"
+                disabled={!user}
               >Enviar Pergunta</Button>
             </div>
           </form>
